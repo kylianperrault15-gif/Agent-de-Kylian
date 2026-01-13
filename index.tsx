d258321dbf653944ce16bce79b669a153cc846ff
kylianperrault15-gif <kylianperrault15@gmail.com>
2026-01-13 16:43:14 +0100

    feat: Refactor to use index.html for entry point
    Migrates the React application's entry point from `index.tsx` to `index.html` by leveraging import maps. This simplifies the build process and enhances portability by making the entire application self-contained within a single HTML file. Also updates dependencies and styling for a darker theme.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,2 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Ce fichier n'est plus nécessaire, tout a été migré dans index.html pour la portabilité
